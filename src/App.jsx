1796d3eed2dcc22c5648d7ecad4e2e0de8608631
NahuelAntunez <nahuelantunez08@gmail.com>
2023-02-06 13:05:46 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,14 @@
 
 import './App.css';
+import NavBar from './componentes/NavBar/NavBar';
+import ItemListContainer from './componentes/ItemListContainer/ItemListContainer';
 
 export const App = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container-fluid">
-      <a className="navbar-brand" href="#">AppVentaDe....</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon" />
-      </button>
-    <div className="collapse navbar-collapse" id="navbarColor01">
-      <ul className="navbar-nav me-auto">
-        <li className="nav-item">
-          <a className="nav-link active" href="#">Home
-            <span className="visually-hidden">(current)</span>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Nosotros</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Contacto</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Productos</a>
-          <div className="dropdown-menu">
-            <a className="dropdown-item" href="#">Mujer</a>
-            <a className="dropdown-item" href="#">Hombre</a>
-            <a className="dropdown-item" href="#">Niñes</a>
-          </div>
-        </li>
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-sm-2" type="search" placeholder="Buscar" />
-        <button className="btn btn-secondary my-2 my-sm-0" type="submit">Buscar</button>
-      </form>
+    <div>
+      <NavBar/>
+      <ItemListContainer parrafo={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."}/>
     </div>
-  </div>
-</nav>
   );
 }
 
